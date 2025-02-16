0885f1772877f2ea953ae567a1818fff2e925622
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-02-16 02:56:27 -0800

    Start on msg creation form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,4 +1,24 @@
 import { Router } from "express";
 const indexRouter = Router();
-indexRouter.get("/", (req, res) => res.render("index"));
+const messages = [
+    {
+        text: "hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "hello world",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+indexRouter.get("/", (req, res) => {
+    res.render("index", { messages: messages });
+});
+indexRouter.get("/new", (req, res) => {
+    res.render("form");
+});
+indexRouter.post("/new", (req, res) => {
+    // hello
+});
 export default indexRouter;
--- routes/indexRouter.ts
+++ routes/indexRouter.ts
@@ -2,6 +2,29 @@ import { Router, Request, Response } from "express";
 
 const indexRouter = Router();
 
-indexRouter.get("/", (req: Request, res: Response) => res.render("index"));
+const messages = [
+  {
+    text: "hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "hello world",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+indexRouter.get("/", (req: Request, res: Response) => {
+  res.render("index", { messages: messages });
+});
+
+indexRouter.get("/new", (req: Request, res: Response) => {
+  res.render("form");
+});
+
+indexRouter.post("/new", (req: Request, res: Response) => {
+  // hello
+});
 
 export default indexRouter;
